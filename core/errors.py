9bd6370fbd49e7d447c2a8efe9f6b2b5492f4f44
carlos.rian <carlos.rian@quartile.com>
2022-06-28 10:02:53 -0300

    update errors.py

--- core/errors.py
+++ core/errors.py
@@ -1,5 +1,10 @@
+import httpx
+
+
 class PrismaError(Exception):
-    ...
+    @property
+    def name(self):
+        return self.__class__.__name__
 
 
 class AlreadyConnectedError(PrismaError):
@@ -8,3 +13,17 @@ class AlreadyConnectedError(PrismaError):
 
 class EngineConnectionError(PrismaError):
     ...
+
+
+class NotConnectedError(PrismaError):
+    ...
+
+
+class EngineRequestError(PrismaError):
+    ...
+
+
+class UnprocessableEntityError(PrismaError):
+    def __init__(self, resp: httpx.Response, *args) -> None:
+        error_msg = f"status_code: {resp.status_code}\n" f"body: {resp.json()}"
+        super().__init__(error_msg, *args)
