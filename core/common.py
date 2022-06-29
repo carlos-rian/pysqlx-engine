6a2ccf90196635c3b5d49bd18231178fafbb38c8
carlos.rian <carlos.rian@quartile.com>
2022-06-29 17:57:49 -0300

    delete 13 files

--- core/common.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-
-def get_open_port() -> int:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(("", 0))
-    port = sock.getsockname()[1]
-    sock.close()
-    return int(port)
