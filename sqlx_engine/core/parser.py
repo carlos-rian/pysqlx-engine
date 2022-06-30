bd257c2b4fb5ae86c91ae6a18af25e62c308b27d
carlos.rian <carlos.rian@quartile.com>
2022-06-30 17:22:42 -0300

    model creation enhancement

--- sqlx_engine/core/parser.py
+++ sqlx_engine/core/parser.py
@@ -31,13 +31,22 @@ TYPES = {
 
 
 class Deserialize:
-    def __init__(self, rows: List[Dict[str, Any]]) -> None:
+    def __init__(self, rows: List[Dict[str, Any]], as_base_row: bool = True) -> None:
         self.rows = rows
+        self.as_base_row = as_base_row
         self._model: BaseRow = None
         self._base_model_type: dict = {}
 
     def deserialize(self):
+        if self.as_base_row:
+            self._create_base_types(self.rows[0])
         return map(self._get_row, self.rows)
+
+    def _create_base_types(self, first_row: dict):
+        for key in first_row.keys():
+            _, _type = self._mapping_type(**first_row[key])
+            self._base_model_type.update({key: (_type, None)})
+        self._create_model()
 
     def _create_model(self) -> None:
         _model = create_model("BaseRow", **self._base_model_type)
@@ -46,14 +55,14 @@ class Deserialize:
     def _get_row(self, row: dict):
         mapper = {}
         for key in row.keys():
-            value, _type = self._mapping_type(**row[key])
-            if not self._model:
-                self._base_model_type.update({key: (_type, None)})
+            value, _ = self._mapping_type(**row[key])
             mapper.update({key: value})
-        if not self._model:
-            self._create_model()
-        return self._model.parse_obj(mapper)
+        if self.as_base_row:
+            return self._model.parse_obj(mapper)
+        return mapper
 
     def _mapping_type(self, prisma__type: TYPES, prisma__value: Any) -> Tuple:
-        _type = TYPES.get(prisma__type, Any)
+        _type = TYPES.get(prisma__type)
+        if _type is None:
+            _type = Any
         return prisma__value, _type
