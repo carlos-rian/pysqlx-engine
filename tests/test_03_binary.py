b35606e35c9bc9a933e0512e1e67dbd38716b157
carlos-rian <crian.rian@gmail.com>
2022-07-10 11:10:49 -0300

    update tests

--- tests/test_03_binary.py
+++ tests/test_03_binary.py
@@ -1,3 +1,10 @@
+from io import StringIO
+from pathlib import Path
+
+from dotenv import load_dotenv
+from sqlx_engine._binary.engine import Engine
+
+
 def test_01_platform_linux():
     from sqlx_engine._binary.platform import binary_platform
 
@@ -15,3 +22,57 @@ def test_02_platform_windows():
     assert name == "windows"
 
     config.platform_name = platform_name
+
+
+def test_03_download_without_dot_binary():
+    engine = Engine()
+    Path(engine._binary_path).unlink(missing_ok=True)
+
+    # remove file and folder where save binary
+    engine.path.unlink(missing_ok=True)
+    engine.path.parent.rmdir()
+
+    assert engine.download() is None
+
+    assert engine.path.exists()
+    assert engine.path.parent.exists()
+
+
+def test_04_download_remove_dot_binary():
+    engine = Engine()
+    Path(engine._binary_path).unlink(missing_ok=True)
+
+    assert engine.download() is None
+    assert engine.path.exists()
+    assert engine.path.parent.exists()
+
+
+def test_05_download_with_dot_binary():
+    engine = Engine()
+
+    assert engine.download() is None
+    assert engine.path.exists()
+    assert engine.path.parent.exists()
+
+
+def test_06_binary_path_dot_binary():
+    engine = Engine()
+
+    path = engine.binary_path
+    assert isinstance(path, str)
+
+
+def test_07_binary_path_dot_binary():
+    engine = Engine()
+    Path(engine._binary_path).unlink(missing_ok=True)
+    path = engine.binary_path
+    assert isinstance(path, str)
+
+
+def test_08_get_path_custom():
+    env = StringIO("DIR_LOCAL_ENGINE_BINARY=.")
+    load_dotenv(stream=env)
+
+    engine = Engine()
+    path = engine.path
+    assert path == Path(".")
