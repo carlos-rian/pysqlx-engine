3fe9cec63aae910621e35091b4dd83b9a0ee9324
carlos-rian <crian.rian@gmail.com>
2022-07-11 15:33:59 -0300

    update engine version

--- sqlx_engine/_config/__init__.py
+++ sqlx_engine/_config/__init__.py
@@ -10,7 +10,8 @@ class Config(BaseConfig):
     improved_error_log: bool = True
     platform_name: str = platform.system().lower()
     method: Literal = Literal["GET", "POST"]
-    engine_version: str = "81a0ee489e5f8c8ce98440028ea9050092430503"
+    engine_version: str = "9f27eed0f8b8773287ff09e657bfa267983e5948"
+    # engine_version: str = "81a0ee489e5f8c8ce98440028ea9050092430503"
     engine_url: str = "https://binaries.prisma.sh/all_commits/{0}/{1}/{2}.gz"
     global_temp_dir: Path = (
         Path(tempfile.gettempdir()) / "prisma" / "binaries" / "engines" / engine_version
