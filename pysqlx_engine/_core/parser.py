cea74ca9ebff0b2fa47ed005b9277a826cd8bad6
carlos-rian <crian.rian@gmail.com>
2022-11-04 18:24:33 -0300

    include support to personal model and method to return columns

--- pysqlx_engine/_core/parser.py
+++ pysqlx_engine/_core/parser.py
@@ -1,6 +1,6 @@
 from datetime import date, datetime, time
 from decimal import Decimal
-from typing import Any, List, Union
+from typing import Any, Dict, List, TypeVar, Union
 from uuid import UUID
 
 from pydantic import BaseModel, create_model, parse_obj_as
@@ -22,6 +22,8 @@ TYPES = {
     "decimal": Decimal,
 }
 
+Model = TypeVar("Model", bound="BaseRow")
+
 
 class BaseRow(BaseModel):
     """
@@ -32,32 +34,56 @@ class BaseRow(BaseModel):
     BaseRow is a class created from `Pydantic`, then you have all the benefits of `Pydantic`.
     """
 
-    ...
+    _columns: Dict[str, Any] = None
+
+    class Config:
+        orm_mode = True
+        ignore_extra = True
+        allow_population_by_field_name = True
+
+    def get_columns(self) -> Dict[str, Any]:
+        """
+        Return the columns of the row.
+
+
+        Returns:
+            Dict[str, Any]: The columns of the row.
+        """
+        return self._columns
 
 
 class Parser:
-    __slots__ = "result"
+    __slots__ = ("result", "model")
 
-    def __init__(self, result: PySQLXResult):
+    def __init__(self, result: PySQLXResult, model: Model = None):
         self.result: PySQLXResult = result
+        self.model: Model = model
 
     def create_model(self) -> BaseRow:
         fields = {}
+        columns = {}
         for key, value in self.result.get_types().items():
             if value.startswith("list_"):
                 _, v = value.split("_")
                 type_ = TYPES.get(v, Any)
                 fields[key] = (List[type_], None)
             else:
-                fields[key] = (TYPES.get(value, Any), None)
-        return create_model("BaseRow", **fields)
+                type_ = TYPES.get(value, Any)
+                fields[key] = (type_, None)
+            columns[key] = type_
+
+        fields["_columns"] = (Dict[str, Any], columns)
+        model = create_model("BaseRow", **fields, __base__=BaseRow)
+        return model
 
     def parse(self) -> List[BaseRow]:
         if len(self.result) == 0:
             return []
-        return parse_obj_as(List[self.create_model()], self.result.get_all())
+        model = self.model or self.create_model()
+        return parse_obj_as(List[model], self.result.get_all())
 
     def parse_first(self) -> Union[BaseRow, None]:
         if len(self.result) == 0:
             return None
-        return parse_obj_as(self.create_model(), self.result.get_first())
+        model = self.model or self.create_model()
+        return parse_obj_as(model, self.result.get_first())
