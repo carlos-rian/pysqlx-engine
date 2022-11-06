e419fcbbd63f116dca36e7741a5683cbe9b8d3c6
carlos-rian <crian.rian@gmail.com>
2022-11-06 12:45:53 -0300

    include doc and method to error

--- pysqlx_engine/_core/errors.py
+++ pysqlx_engine/_core/errors.py
@@ -19,6 +19,8 @@ from .helper import fe_json
 
 
 class PySQLXError(Exception):
+    """Base class for all PySQLXEngine errors."""
+
     def __init__(self, err: _PySQLXError, *args: object):
         self.code: str = err.code()
         self.message: str = err.message()
@@ -37,38 +39,78 @@ class PySQLXError(Exception):
 
         super().__init__(msg)
 
+    def code(self) -> str:
+        """Return the error code."""
+        return self.code
+
+    def message(self) -> str:
+        """Return the error message."""
+        return self.message
+
+    def error(self) -> str:
+        """Return the error type."""
+        return self._type
+
 
 class QueryError(PySQLXError):
+    """
+    Raised when a query fails.
+    """
+
     ...
 
 
 class ExecuteError(PySQLXError):
+    """
+    Raised when an error occurs while executing a query or invalid sql.
+    """
+
     ...
 
 
 class ConnectError(PySQLXError):
+    """Raised when a connection error occurs."""
+
     ...
 
 
 class IsoLevelError(PySQLXError):
+    """
+    Raised when the isolation level is not supported or not valid
+    """
+
     ...
 
 
 class StartTransactionError(PySQLXError):
+    """
+    Raised when the user tries to start a transaction
+    while there is already an active transaction for example.
+    """
+
     ...
 
 
 class RawCmdError(PySQLXError):
+    """
+    Raised when the user tries to execute a raw command
+
+    """
+
     ...
 
 
 class NotConnectedError(Exception):
+    """
+    Raised when the user tries to execute a sql but is not connected to the database.
+    """
+
     ...
 
 
 class AlreadyConnectedError(ValueError):
-    ...
+    """
+    Raised when the user tries to connect to the database but is already connected.
+    """
 
-
-class NotConnectedError(ValueError):
     ...
