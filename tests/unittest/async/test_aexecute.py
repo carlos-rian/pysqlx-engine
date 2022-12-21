432088c9d657d54fabe0bd8111f4917cb3edbcce
carlos-rian <crian.rian@gmail.com>
2022-12-21 10:27:57 -0300

    incomple test

--- tests/unittest/async/test_aexecute.py
+++ tests/unittest/async/test_aexecute.py
@@ -65,3 +65,38 @@ async def test_error_execute_invalid_table_insert(db):
 
     await conn.close()
     assert conn.connected is False
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("db", [adb_mysql])
+async def test_execute_sql_with_complex_param_mysql(db: PySQLXEngine):
+    conn: PySQLXEngine = await db()
+    assert conn.connected is True
+
+    from tests.unittest.sql.mysql.value import data
+
+    await conn.execute(sql="DROP TABLE IF EXISTS pysqlx_table;")
+
+    with open("tests/unittest/sql/mysql/create.sql", "r") as f:
+        sql = f.read()
+        resp = await conn.execute(sql=sql)
+        assert resp == 0
+
+    with open("tests/unittest/sql/mysql/insert.sql", "r") as f:
+        sql = f.read()
+        resp = await conn.execute(sql=sql, parameters=data)
+        assert resp == 1
+    from pysqlx_engine import BaseRow
+
+    class Model(BaseRow):
+        id: int
+        name: str
+        age: int
+        address: str
+        salary: float
+
+    resp = await conn.query_first(sql="SELECT * FROM pysqlx_table")
+
+    resp = await conn.execute(sql="DROP TABLE IF EXISTS pysqlx_table;")
+    assert resp == 0
+    await conn.close()
