5b1810687c53b995fab5fc57b6df9acc756af6e7
carlos-rian <crian.rian@gmail.com>
2022-12-24 18:52:13 -0300

    include complex test with parameter to mysql

--- tests/unittest/async/test_aexecute.py
+++ tests/unittest/async/test_aexecute.py
@@ -1,6 +1,7 @@
 import pytest
 
 from pysqlx_engine import PySQLXEngine
+from decimal import Decimal
 from pysqlx_engine.errors import ExecuteError
 from tests.common import adb_mssql, adb_mysql, adb_pgsql, adb_sqlite
 
@@ -69,11 +70,13 @@ async def test_error_execute_invalid_table_insert(db):
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("db", [adb_mysql])
-async def test_execute_sql_with_complex_param_mysql(db: PySQLXEngine):
+async def test_execute_sql_with_complex_param(db: PySQLXEngine):
     conn: PySQLXEngine = await db()
     assert conn.connected is True
 
     from tests.unittest.sql.mysql.value import data
+    from datetime import date, datetime, time
+    from decimal import Decimal
 
     await conn.execute(sql="DROP TABLE IF EXISTS pysqlx_table;")
 
@@ -86,16 +89,28 @@ async def test_execute_sql_with_complex_param_mysql(db: PySQLXEngine):
         sql = f.read()
         resp = await conn.execute(sql=sql, parameters=data)
         assert resp == 1
-    from pysqlx_engine import BaseRow
-
-    class Model(BaseRow):
-        id: int
-        name: str
-        age: int
-        address: str
-        salary: float
 
     resp = await conn.query_first(sql="SELECT * FROM pysqlx_table")
+
+    assert isinstance(resp.type_int, int)
+    assert isinstance(resp.type_smallint, int)
+    assert isinstance(resp.type_bigint, int)
+    assert isinstance(resp.type_numeric, Decimal)
+    assert isinstance(resp.type_decimal, Decimal)
+    assert isinstance(resp.type_float, float)
+    assert isinstance(resp.type_double, float)
+    assert isinstance(resp.type_char, str)
+    assert isinstance(resp.type_varchar, str)
+    assert isinstance(resp.type_nvarchar, str)
+    assert isinstance(resp.type_text, str)
+    assert isinstance(resp.type_boolean, bool)
+    assert isinstance(resp.type_date, (date, datetime))  # type: ignore is wrong, Quaint returns datetime for date
+    assert isinstance(resp.type_time, time)
+    assert isinstance(resp.type_timestamp, datetime)
+    assert isinstance(resp.type_datetime, datetime)
+    assert isinstance(resp.type_enum, str)
+    assert isinstance(resp.type_json, (dict, list))
+    assert isinstance(resp.type_bytes, bytes)
 
     resp = await conn.execute(sql="DROP TABLE IF EXISTS pysqlx_table;")
     assert resp == 0
