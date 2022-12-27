d1ed6315d0a6d74deab49b25b3df837f1e70931f
carlos-rian <crian.rian@gmail.com>
2022-12-27 15:08:29 -0300

    include json parse to dumps using class.

--- pysqlx_engine/_core/param.py
+++ pysqlx_engine/_core/param.py
@@ -1,6 +1,6 @@
 from datetime import date, datetime, time
 from decimal import Decimal
-from json import dumps, loads, JSONEncoder
+import json
 from typing import Union, Dict, List, Any, Tuple, Type, Callable
 from uuid import UUID
 from functools import lru_cache
@@ -11,7 +11,7 @@ from .errors import ParameterInvalidProviderError, ParameterInvalidValueError
 
 @lru_cache(maxsize=None)
 def try_bool(provider: PROVIDER, value: bool, _f: str = "") -> str:
-    if provider == "sqlserver":
+    if provider.startswith("sqlserver") or provider.startswith("mysql"):
         return "1" if value else "0"
     return str(value).upper()
 
@@ -22,13 +22,13 @@ def try_str(_p: PROVIDER, value: str, _f: str = "") -> str:
 
 
 @lru_cache(maxsize=None)
-def try_int(_p: PROVIDER, value: int, _f: str = "") -> str:
-    return f"{value}"
+def try_int(_p: PROVIDER, value: int, _f: str = "") -> int:
+    return value
 
 
 def try_json(provider: PROVIDER, value: Union[Dict[str, Any], List[Dict[str, Any]]], field: str = "") -> str:
     try:
-        return f"'{dumps(value, ensure_ascii=False, default=str)}'"
+        return f"'{json.dumps(value, ensure_ascii=False, cls=PySQLXJsonEnconder)}'"
     except Exception as err:
         raise ParameterInvalidValueError(
             field=field, provider=provider, typ_from="dict|list", typ_to="json", details=str(err)
@@ -56,8 +56,8 @@ def try_datetime(_p: PROVIDER, value: datetime, _f: str = "") -> str:
 
 
 @lru_cache(maxsize=None)
-def try_float(_p: PROVIDER, value: float, _f: str = "") -> str:
-    return f"{value}"
+def try_float(_p: PROVIDER, value: float, _f: str = "") -> float:
+    return value
 
 
 @lru_cache(maxsize=None)
@@ -71,43 +71,55 @@ def try_bytes(provider: PROVIDER, value: bytes, field: str = "") -> str:
     elif provider == "mysql":
         return f"X'{value.hex()}'"
 
-    elif provider == "sqlite":
-        return f"x'{value.hex()}'"
-
-    raise ParameterInvalidProviderError(field=field, provider=provider, typ="bytes")
+    # sqlite
+    return f"x'{value.hex()}'"
 
 
 @lru_cache(maxsize=None)
-def try_decimal(provider: PROVIDER, value: Decimal, field: str = "") -> str:
-    try:
-        return f"'{str(value)}'"
-    except Exception as err:
-        raise ParameterInvalidValueError(
-            field=field, provider=provider, typ_to="decimal", typ_from="str", details=str(err)
-        )
+def try_decimal(_p: PROVIDER, value: Decimal, _f: str = "") -> str:
+    return f"'{str(value)}'"
 
 
 @lru_cache(maxsize=None)
 def try_tuple(provider: PROVIDER, values: Tuple[Any], field: str = "") -> str:
-    try:
-        types = set([type(v) for v in values])
+    if not provider.startswith("postgresql"):
+        raise ParameterInvalidProviderError(field=field, provider=provider, typ="array")
 
-        if len(types) > 1:
-            raise ParameterInvalidValueError(
-                field=field,
-                provider=provider,
-                typ_from="tuple",
-                typ_to="sql array",
-                details="the tuple must be of the same type, eg: (1, 2, 3). tuple is represented as a array in sql, sql does not support heterogeneous tuple.",
-            )
-        if len(values) > 0:
-            method = get_method(typ=types.pop())
-            data = str([method(v) for v in values]).replace("[", "{").replace("]", "}")
-            return f"'{data}'"
-        return "'{}'"
+    types = set([type(v) for v in values])
 
-    except Exception as err:
-        raise ParameterInvalidValueError(field=field, provider=provider, typ="list", typ_from="str", details=str(err))
+    if len(types) > 1:
+        raise ParameterInvalidValueError(
+            field=field,
+            provider=provider,
+            typ_from="tuple",
+            typ_to="array",
+            details=(
+                "the tuple must be of the same type, eg: (1, 2, 3). "
+                "tuple is represented as a array in sql, sql does not support heterogeneous array."
+            ),
+        )
+
+    if len(values) > 0:
+        typ_ = types.pop()
+        method = get_method(typ=typ_)
+
+        if method is None:
+            raise ParameterInvalidProviderError(field=field, provider=provider, typ=typ_)
+
+        data = str([method(provider, value, field) for value in values]).replace("[", "{").replace("]", "}")
+        return f"'{data}'"
+    return "'{}'"
+
+
+class PySQLXJsonEnconder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, bytes):
+            return obj.hex()
+
+        elif isinstance(obj, (UUID, time, date, datetime, Decimal)):
+            return str(obj)
+
+        return super().default(obj)
 
 
 def get_method(typ: Type) -> Callable:
@@ -127,7 +139,7 @@ def get_method(typ: Type) -> Callable:
         Decimal: try_decimal,
     }
 
-    return METHODS.get(typ, try_str)
+    return METHODS.get(typ)
 
 
 def convert(
@@ -136,7 +148,7 @@ def convert(
         bool, str, int, Dict[str, Any], List[Dict[str, Any]], UUID, time, date, datetime, float, bytes, Decimal, None
     ],
     field: str = "",
-) -> str:
+) -> Union[str, int, float]:
     if value is None:
         return "NULL"
 
@@ -144,6 +156,12 @@ def convert(
 
     method = get_method(typ=typ_)
     if method is None:
-        raise ParameterInvalidProviderError(field=field, provider=provider, typ=typ_)
+        raise ParameterInvalidValueError(
+            field=field,
+            provider=provider,
+            typ_from=typ_,
+            typ_to="str|int|float",
+            details="invalid type, the value is not a allowed type.",
+        )
 
     return method(provider, value, field)
