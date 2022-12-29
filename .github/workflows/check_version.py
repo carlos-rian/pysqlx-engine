4cf508212953f78d40d0719fec84c4b5ae0ed321
carlos-rian <crian.rian@gmail.com>
2022-12-28 21:37:42 -0300

    try create beta version

---
+++ .github/workflows/check_version.py
@@ -0,0 +1,34 @@
+import httpx
+import toml
+
+with open("pyproject.toml", mode="r") as file:
+    text: str = file.read()
+
+
+def get_version():
+    uri = "https://pypi.org/pypi/pysqlx-engine/json"
+    for _ in range(3):
+        resp = httpx.get(uri)
+        if resp.is_success:
+            break
+    json: dict = resp.json()
+    return json["info"]["version"]
+
+
+file_version = toml.loads(text)["tool"]["poetry"]["version"]
+
+version: str = get_version()
+print("Package version:", version)
+
+MAJOR, MINOR, PATCH = version.split(".")
+
+PATCH = int(PATCH) + 1
+
+new_version: str = ".".join([MAJOR, MINOR, str(PATCH)]) + "b"
+
+print("Package new version:", new_version)
+
+new_text = text.replace(f'version = "{file_version}"', f'version = "{new_version}"')
+
+with open("pyproject.toml", mode="w") as file:
+    file.write(new_text)
