3fddd3de35785026c1e023825fd1e43845bd4230
carlos.rian <carlos.rian@quartile.com>
2024-07-20 13:37:30 -0300

    feat: Refactor force_sync function in pysqlx_engine

--- pysqlx_engine/_core/util.py
+++ pysqlx_engine/_core/util.py
@@ -53,10 +53,12 @@ def force_sync(fn: Callable[P, T]) -> Callable[P, T]:
         loop = asyncio.get_event_loop()
 
         # if loop is already running, we don't need to run it again
-        if loop.is_running():
-            return res
+        if not loop.is_running():
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+
         # if the result is a coroutine, we need to run it in the loop synchronously
-        elif asyncio.iscoroutine(res):
+        if asyncio.iscoroutine(res):
             return loop.run_until_complete(res)
 
         # case the result is not a coroutine, we just return it
@@ -107,24 +109,42 @@ def check_sql_and_parameters(sql: str, parameters: dict):
         if (
             not isinstance(parameters, dict)
             or not all([isinstance(key, str) for key in parameters.keys()])
-            or not all([isinstance(value, _types) or value is None for value in parameters.values()])
+            or not all(
+                [
+                    isinstance(value, _types) or value is None
+                    for value in parameters.values()
+                ]
+            )
         ):
             raise TypeError(parameters_type_error_message())
 
 
 @lru_cache(maxsize=None)
 def check_isolation_level(isolation_level: ISOLATION_LEVEL):
-    levels = ["ReadUncommitted", "ReadCommitted", "RepeatableRead", "Snapshot", "Serializable"]
-    if not isinstance(isolation_level, str) or not any([isolation_level == level for level in levels]):
+    levels = [
+        "ReadUncommitted",
+        "ReadCommitted",
+        "RepeatableRead",
+        "Snapshot",
+        "Serializable",
+    ]
+    if not isinstance(isolation_level, str) or not any(
+        [isolation_level == level for level in levels]
+    ):
         raise ValueError(isolation_error_message())
 
 
 def build_sql(provider: str, sql: str, parameters: dict = None) -> str:
     new_sql = sql
     if parameters is not None:
-        load_parameter = {key: convert(provider=provider, value=value, field=key) for key, value in parameters.items()}
+        load_parameter = {
+            key: convert(provider=provider, value=value, field=key)
+            for key, value in parameters.items()
+        }
 
-        param_as_list_of_tuples = sorted(load_parameter.items(), key=lambda x: x[0], reverse=True)
+        param_as_list_of_tuples = sorted(
+            load_parameter.items(), key=lambda x: x[0], reverse=True
+        )
 
         for key, value in param_as_list_of_tuples:
             new_sql = new_sql.replace(f":{key}", str(value))
@@ -136,5 +156,11 @@ def build_sql(provider: str, sql: str, parameters: dict = None) -> str:
     return new_sql
 
 
-def parse_obj_as(type_: Union[BaseModel, List[BaseModel]], obj: Union[dict, list]) -> type:
-    return _parse_obj_as(type_=type_, obj=obj) if PYDANTIC_IS_V1 else TypeAdapter(type=type_).validate_python(obj)
+def parse_obj_as(
+    type_: Union[BaseModel, List[BaseModel]], obj: Union[dict, list]
+) -> type:
+    return (
+        _parse_obj_as(type_=type_, obj=obj)
+        if PYDANTIC_IS_V1
+        else TypeAdapter(type=type_).validate_python(obj)
+    )
