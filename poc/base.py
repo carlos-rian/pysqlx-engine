683a911c6d5ce45896273ffa01bdc574602804a8
carlos.rian <carlos.rian@quartile.com>
2024-07-20 18:55:29 -0300

    feat: Add PoolAlreadyStarted exception to errors module

--- poc/base.py
+++ poc/base.py
@@ -1,5 +1,5 @@
 import logging
-from abc import ABC, ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 from collections import deque as Deque
 from random import random
 from time import monotonic
@@ -10,29 +10,43 @@ from typing_extensions import TypeAlias
 
 from pysqlx_engine import PySQLXEngine, PySQLXEngineSync
 
+from .errors import PoolClosed
+
 logger = logging.getLogger("pysqlx_engine")
 TPySQLXEngineConn: TypeAlias = Union[PySQLXEngine, PySQLXEngineSync]
 
 
-class ConnInfo(ABCMeta):
+class BaseConnInfo(ABC):
 	_num_conn: int = 0
 
 	def __init__(self, conn: TPySQLXEngineConn, keep_alive: float):
-		self._num_conn = ConnInfo._num_conn = ConnInfo._num_conn + 1
+		self._num_conn = BaseConnInfo._num_conn = BaseConnInfo._num_conn + 1
 		self.name = f"conn-{self._num_conn}"
 		self.conn = conn
 
-		self.expire_at = monotonic() + self._jitter(value=keep_alive, min_pc=-0.05, max_pc=0.0)
+		self.keep_alive = keep_alive
+		self.expire_at = monotonic() + self._jitter(value=self.keep_alive, min_pc=-0.05, max_pc=0.0)
 		self.start_at = monotonic()
 
 	def __repr__(self) -> str:
 		return f"<{self.__class__.__module__}.{self.__class__.__name__} {self.name!r} at 0x{id(self):x}>"
 
+	@property
+	def _can_reuse(self) -> bool:
+		finish = self.start_at + (self.keep_alive * 4)
+		return finish > monotonic()
+
+	@property
 	def expires(self) -> bool:
 		return self.expire_at < monotonic()
 
+	@property
 	def healthy(self) -> bool:
 		return self.conn.is_healthy()
+
+	@property
+	def reusable(self) -> bool:
+		return self.healthy and self.conn.connected and self._can_reuse
 
 	@abstractmethod
 	def close(self) -> None: ...
@@ -47,6 +61,9 @@ class ConnInfo(ABCMeta):
 		self.expire_at = monotonic()
 		print(f"Removed: {self} from pool, the conn was open for {self.expire_at - self.start_at} secs")
 
+	def renew_expire_at(self):
+		self.expire_at = monotonic() + self._jitter(value=self.keep_alive, min_pc=-0.05, max_pc=0.0)
+
 	@classmethod
 	def _jitter(cls, value: float, min_pc: float, max_pc: float) -> float:
 		"""
@@ -55,7 +72,7 @@ class ConnInfo(ABCMeta):
 		return value * (1.0 + ((max_pc - min_pc) * random()) + min_pc)
 
 
-class BasePool(ABCMeta):
+class BasePool(ABC):
 	_num_pool = 0
 
 	def __init__(
@@ -64,7 +81,7 @@ class BasePool(ABCMeta):
 		min_size: int,
 		max_size: int = None,
 		conn_timeout: float = 30.0,
-		max_lifetime: float = 60 * 60,
+		keep_alive: float = 60 * 15,
 		check_interval: float = 5.0,
 	):
 		"""
@@ -80,22 +97,22 @@ class BasePool(ABCMeta):
 
 		self.uri = uri
 		self._conn_timeout = conn_timeout or 30.0
-		self._max_lifetime = max_lifetime or 60 * 60
+		self._keep_alive = keep_alive or 60 * 15
 		self._check_interval = check_interval or 5.0
 
 		assert self._conn_timeout > 0, "conn_timeout must be greater than 0"
-		assert self._max_lifetime > 0, "max_lifetime must be greater than 0"
+		assert self._keep_alive > 0, "max_lifetime must be greater than 0"
 		assert check_interval > 0, "check_interval must be greater than 0"
 
-		if self._max_lifetime < 60:
+		if self._keep_alive < 60:
 			logger.warning("max_lifetime is less than 60 seconds, this is not recommended")
 
 		self._num_pool = BasePool._num_pool = BasePool._num_pool + 1
 		self._name = f"{self.__class__.__name__}-{self._num_pool}"
 
 		self._size = 0
-		self._pool = Deque()
-		self._closed = False
+		self._pool: Deque[BaseConnInfo] = Deque()
+		self._opened = False
 
 		self._min_size, self._max_size = self._check_size(min_size, max_size)
 
@@ -104,7 +121,7 @@ class BasePool(ABCMeta):
 
 	@property
 	def closed(self) -> bool:
-		return self._closed
+		return self._opened is False
 
 	def _check_size(self, min_size: int, max_size: Union[int | None]):
 		if max_size is None:
@@ -120,19 +137,34 @@ class BasePool(ABCMeta):
 
 	def _check_closed(self) -> None:
 		if self.closed:
-			raise ValueError("Pool is closed")
+			raise PoolClosed("Pool is closed")
+
+	@abstractmethod
+	def _new_conn(self) -> BaseConnInfo: ...
+
+	@abstractmethod
+	def _del_conn(self, conn: BaseConnInfo) -> None: ...
+
+	@abstractmethod
+	def _put_conn(self, conn: BaseConnInfo) -> None: ...
+
+	@abstractmethod
+	def _get_ready_conn(self) -> BaseConnInfo: ...
+
+	@abstractmethod
+	def _get_conn(self) -> BaseConnInfo: ...
+
+	@abstractmethod
+	def _start(self) -> None: ...
 
 	@abstractmethod
 	def start(self) -> None: ...
 
 	@abstractmethod
+	def get_connection(self) -> TPySQLXEngineConn: ...
+
+	@abstractmethod
 	def stop(self) -> None: ...
-
-	@abstractmethod
-	def _put_conn(self, conn: ConnInfo) -> None: ...
-
-	@abstractmethod
-	def _get_conn(self) -> ConnInfo: ...
 
 
 class BaseMonitor:
--- poc/test_pool.py
+++ poc/test_pool.py
@@ -1,9 +1,13 @@
 import asyncio
 import threading
+from contextlib import asynccontextmanager
+from time import monotonic
 from weakref import ReferenceType, ref
 
-from .base import BaseMonitor, BasePool, ConnInfo
-from .errors import PoolClosed, PoolTimeout
+from pysqlx_engine._core.aconn import PySQLXEngine
+
+from .base import BaseConnInfo, BaseMonitor, BasePool
+from .errors import PoolAlreadyStarted, PoolClosed, PoolTimeout
 
 
 def current_thread_name() -> str:
@@ -13,6 +17,110 @@ def current_thread_name() -> str:
 def current_task_name() -> str:
 	t = asyncio.current_task()
 	return t.get_name() if t else "<no task>"
+
+
+class ConnInfo(BaseConnInfo):
+	async def close(self) -> None:
+		return await super()._aclose()
+
+
+class ConnInfoSync(BaseConnInfo):
+	def close(self) -> None:
+		return super()._close()
+
+
+class Pool(BasePool):
+	def __init__(
+		self,
+		uri: str,
+		min_size: int,
+		max_size: int = None,
+		conn_timeout: float = 30.0,
+		keep_alive: float = 60 * 15,
+		check_interval: float = 5.0,
+	):
+		super().__init__(
+			uri=uri,
+			min_size=min_size,
+			max_size=max_size,
+			conn_timeout=conn_timeout,
+			keep_alive=keep_alive,
+			check_interval=check_interval,
+		)
+		self._lock = asyncio.Lock()
+
+	def __del__(self) -> None:
+		if getattr(self, "_pool", None):
+			asyncio.run(self.stop())
+
+	async def _new_conn(self) -> BaseConnInfo:
+		conn = PySQLXEngine(uri=self.uri)
+		await conn.connect()
+		self._size += 1
+		return ConnInfo(conn=conn, keep_alive=self._keep_alive)
+
+	async def _del_conn(self, conn: BaseConnInfo) -> None:
+		await conn.close()
+		self._size -= 1
+
+	async def _put_conn(self, conn: BaseConnInfo) -> None:
+		self._check_closed()
+		if not conn.reusable:
+			await self._del_conn(conn)
+			conn = await self._new_conn()
+
+		self._pool.append(conn)
+		self._num_pool += 1
+
+	async def _get_ready_conn(self) -> BaseConnInfo:
+		if self._pool:
+			conn = self._pool.popleft()
+			return conn
+
+		if self._size < self._max_size:
+			conn = await self._new_conn()
+			return conn
+
+	async def _get_conn(self) -> BaseConnInfo:
+		self._check_closed()
+		deadline = monotonic() + self._conn_timeout
+
+		while True:
+			timeout = deadline - monotonic()
+
+			if timeout < 0.0:
+				raise PoolTimeout("Timeout waiting for a connection")
+
+			conn = await self._get_ready_conn()
+			if conn:
+				return conn
+
+			await asyncio.sleep(0.1)
+
+	async def _start(self) -> None:
+		if self._num_pool > 0:
+			raise PoolAlreadyStarted("Pool is already started")
+
+		async with self._lock:
+			for _ in range(self._min_size):
+				conn = await self._new_conn()
+				await self._put_conn(conn)
+			self._opened = True
+
+	@asynccontextmanager
+	async def get_connection(self):
+		conn = await self._get_conn()
+		try:
+			yield conn.conn
+		finally:
+			await self._put_conn(conn)
+
+	async def stop(self) -> None:
+		async with self._lock:
+			self._opened = False
+			while self._pool:
+				conn = self._pool.popleft()
+				await self._del_conn(conn)
 
 
 class Monitor(BaseMonitor):
@@ -27,8 +135,22 @@ class Monitor(BaseMonitor):
 		while True:
 			async with self._lock:
 				pool = self.pool()
-				if pool._pool:
-					...
+				for _ in range(len(pool._pool)):
+					conn = pool._pool.popleft()
+					if conn.healthy is False:
+						await pool._del_conn(conn=conn)
+
+					elif (pool._min_size > pool._size < pool._max_size) or pool._size > pool._max_size:
+						# close the connection
+						await pool._del_conn(conn=conn)
+
+					elif conn.expires:
+						# reuse the connection
+						conn.renew_expire_at()
+						await pool._put_conn(conn=conn)
+
+					else:
+						await pool._put_conn(conn=conn)
 
 			await asyncio.sleep(pool._check_interval)
 
