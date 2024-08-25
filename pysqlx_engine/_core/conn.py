21875ebce7629f14b6adfaf30df7ed6101639cad
carlos.rian <carlos.rian@quartile.com>
2024-08-25 13:59:25 -0300

    use native sync support to PySQLXEngineSync

--- pysqlx_engine/_core/conn.py
+++ pysqlx_engine/_core/conn.py
@@ -1,17 +1,55 @@
 from typing import Optional
 
-from .aconn import PySQLXEngine as _PySQLXEngine
-from .const import ISOLATION_LEVEL
-from .parser import MyModel  # import necessary using _core to not subscribe default parser
-from .util import force_sync
+import pysqlx_core
+
+# ParserSQL,
+# import necessary using _core to not subscribe default parser
+from .const import ISOLATION_LEVEL, LOG_CONFIG
+from .errors import AlreadyConnectedError, NotConnectedError, ParameterInvalidValueError
+from .helper import fe_sql, model_parameter_error_message, not_connected_error_message
+from .logger import logger
+from .parser import BaseRow, MyModel, ParserIn, ParserSQL
+from .util import check_isolation_level, check_sql_and_parameters, create_log_line, pysqlx_get_error
 
 
-class PySQLXEngineSync(_PySQLXEngine):
+class PySQLXEngineSync:
+	__slots__ = ["uri", "connected", "_conn", "_provider"]
+
 	uri: str
 	connected: bool
 
 	def __init__(self, uri: str):
-		super().__init__(uri=uri)
+		self.connected: bool = False
+
+		_providers = ["postgresql", "mysql", "sqlserver", "sqlite"]
+		if not uri or not any([uri.startswith(prov) for prov in [*_providers, "file"]]):
+			raise ValueError(f"invalid uri: {uri}, check the usage uri, accepted providers: {', '.join(_providers)}")
+
+		if uri.startswith("sqlite"):
+			uri = uri.replace("sqlite", "file", 1)
+
+		self.uri: str = uri
+		self._conn: Optional[pysqlx_core.Connection] = None
+
+		self._provider = "sqlite"
+
+		for prov in _providers:
+			if self.uri.startswith(prov):
+				self._provider = prov
+
+	def __del__(self):
+		if self.connected:
+			del self._conn
+			self._conn = None
+			self.connected = False
+
+	def is_healthy(self):
+		self._pre_validate()
+		return self._conn.is_healthy()
+
+	def requires_isolation_first(self):
+		self._pre_validate()
+		return self._conn.requires_isolation_first()
 
 	def __enter__(self):
 		self.connect()
@@ -20,54 +58,126 @@ class PySQLXEngineSync(_PySQLXEngine):
 	def __exit__(self, exc_type, exc, exc_tb):
 		self.close()
 
-	@force_sync
-	async def connect(self):
-		await (super()).connect()
+	def connect(self):
+		if self.connected:
+			raise AlreadyConnectedError()
+		try:
+			self._conn = pysqlx_core.new_sync(uri=self.uri)
+			self.connected = True
+		except pysqlx_core.PySQLxError as e:
+			raise pysqlx_get_error(err=e)
 
-	@force_sync
-	async def close(self):
-		await (super()).close()
+	def close(self):
+		if self.connected:
+			del self._conn
+			self._conn = None
+			self.connected = False
 
-	@force_sync
-	async def raw_cmd(self, sql: str):
-		return await (super()).raw_cmd(sql=sql)
+	def _dev_mode(self, sql: str, parameters: Optional[dict] = None):
+		logger.debug(create_log_line(" PYSQLX-ENGINE DEVELOPMENT MODE "))
+		logger.debug(
+			create_log_line(
+				"THIS SQL STATEMENT IS PRE BUILD JUST FOR DEVELOPMENT PURPOSE, "
+				"ALL SQL AND PARAMS WILL PASS TO DATABASE."
+			)
+		)
+		log_sql = ParserSQL(provider=self._provider, sql=sql, parameters=parameters).sql()
+		logger.debug(fe_sql(sql=log_sql))
 
-	@force_sync
-	async def query(self, sql: str, parameters: Optional[dict] = None, model: Optional[MyModel] = None):
-		return await (super()).query(sql=sql, parameters=parameters, model=model)
+	def _run(self, func, sql: str, parameters: Optional[dict] = None, model: Optional[MyModel] = None):
+		if model is not None and not issubclass(model, BaseRow):
+			raise TypeError(model_parameter_error_message())
 
-	@force_sync
-	async def query_as_dict(self, sql: str, parameters: Optional[dict] = None):
-		return await (super()).query_as_dict(sql=sql, parameters=parameters)
+		try:
+			stmt = pysqlx_core.PySQLxStatement(provider=self._provider, sql=sql, params=parameters)
+			if LOG_CONFIG.PYSQLX_DEV_MODE:
+				self._dev_mode(sql=sql, parameters=parameters)
 
-	@force_sync
-	async def query_first(self, sql: str, parameters: Optional[dict] = None, model: Optional[MyModel] = None):
-		return await (super()).query_first(sql=sql, parameters=parameters, model=model)
+			if LOG_CONFIG.PYSQLX_SQL_LOG:
+				logger.debug(create_log_line(" THIS SQL IS THE FINAL SQL AND PARAMS STATEMENT THAT WILL BE EXECUTED "))
+				logger.info(f"\nSQL: {fe_sql(stmt.sql())}\nPARAMS: {stmt.params()}")
 
-	@force_sync
-	async def query_first_as_dict(self, sql: str, parameters: Optional[dict] = None):
-		return await (super()).query_first_as_dict(sql=sql, parameters=parameters)
+			return func(stmt)
 
-	@force_sync
-	async def execute(self, sql: str, parameters: Optional[dict] = None):
-		return await (super()).execute(sql=sql, parameters=parameters)
+		except pysqlx_core.PySQLxError as e:
+			raise pysqlx_get_error(err=e)
 
-	@force_sync
-	async def set_isolation_level(self, isolation_level: ISOLATION_LEVEL):
-		await (super()).set_isolation_level(isolation_level=isolation_level)
+		except pysqlx_core.PySQLxInvalidParamError as e:
+			raise ParameterInvalidValueError(
+				field=e.field(),
+				provider=self._provider,
+				typ_from=e.typ_from(),
+				typ_to=e.typ_to(),
+			)
 
-	@force_sync
-	async def begin(self):
-		await (super()).begin()
+	def raw_cmd(self, sql: str):
+		return self._run(func=self._conn.raw_cmd_sync, sql=sql)
 
-	@force_sync
-	async def commit(self):
-		await (super()).commit()
+	def query(self, sql: str, parameters: Optional[dict] = None, model: Optional[MyModel] = None):
+		self._pre_validate(sql=sql, parameters=parameters)
+		result = self._run(func=self._conn.query_typed_sync, sql=sql, parameters=parameters, model=model)
+		return ParserIn(result=result, model=model).parse()
 
-	@force_sync
-	async def rollback(self):
-		await (super()).rollback()
+	def query_as_dict(self, sql: str, parameters: Optional[dict] = None):
+		self._pre_validate(sql=sql, parameters=parameters)
+		return self._run(self._conn.query_all_sync, sql=sql, parameters=parameters)
 
-	@force_sync
-	async def start_transaction(self, isolation_level: Optional[ISOLATION_LEVEL] = None):
-		await (super()).start_transaction(isolation_level=isolation_level)
+	def query_first(self, sql: str, parameters: Optional[dict] = None, model: Optional[MyModel] = None):
+		self._pre_validate(sql=sql, parameters=parameters)
+		result = self._run(self._conn.query_typed_sync, sql=sql, parameters=parameters, model=model)
+		return ParserIn(result=result, model=model).parse_first()
+
+	def query_first_as_dict(self, sql: str, parameters: Optional[dict] = None):
+		self._pre_validate(sql=sql, parameters=parameters)
+		row = self._run(self._conn.query_one_sync, sql=sql, parameters=parameters)
+		return row if row else None
+
+	def execute(self, sql: str, parameters: Optional[dict] = None):
+		self._pre_validate(sql=sql, parameters=parameters)
+		return self._run(self._conn.execute_sync, sql=sql, parameters=parameters)
+
+	def set_isolation_level(self, isolation_level: ISOLATION_LEVEL):
+		self._pre_validate(isolation_level=isolation_level)
+		try:
+			self._conn.set_isolation_level_sync(isolation_level=isolation_level)
+		except pysqlx_core.PySQLxError as e:
+			raise pysqlx_get_error(err=e)
+
+	def begin(self):
+		self.start_transaction()
+
+	def commit(self):
+		self._pre_validate()
+		if self._provider == "sqlserver":
+			self.raw_cmd(sql="COMMIT TRANSACTION;")
+		else:
+			self.raw_cmd(sql="COMMIT;")
+
+	def rollback(self):
+		self._pre_validate()
+		if self._provider == "sqlserver":
+			self.raw_cmd(sql="ROLLBACK TRANSACTION;")
+		else:
+			self.raw_cmd(sql="ROLLBACK;")
+
+	def start_transaction(self, isolation_level: Optional[ISOLATION_LEVEL] = None):
+		self._pre_validate(isolation_level=isolation_level)
+		try:
+			self._conn.start_transaction_sync(isolation_level=isolation_level)
+		except pysqlx_core.PySQLxError as e:
+			raise pysqlx_get_error(err=e)
+
+	def _pre_validate(
+		self,
+		sql: str = "",
+		parameters: Optional[dict] = None,
+		isolation_level: Optional[ISOLATION_LEVEL] = None,
+	):
+		if not self.connected:
+			raise NotConnectedError(not_connected_error_message())
+
+		if sql != "":
+			check_sql_and_parameters(sql=sql, parameters=parameters)
+
+		if isolation_level is not None:
+			check_isolation_level(isolation_level=isolation_level)
