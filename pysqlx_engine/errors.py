557172257bb4e8e64e113a7959b4023d9de52a37
carlos.rian <carlos.rian@quartile.com>
2024-08-24 21:05:14 -0300

    Refactor pysqlx_engine/errors.py to add new pool errors

--- pysqlx_engine/errors.py
+++ pysqlx_engine/errors.py
@@ -1,3 +1,4 @@
+# new pool errors
 from ._core.errors import AlreadyConnectedError as AlreadyConnectedError
 from ._core.errors import ConnectError as ConnectError
 from ._core.errors import ExecuteError as ExecuteError
@@ -6,7 +7,9 @@ from ._core.errors import NotConnectedError as NotConnectedError
 from ._core.errors import ParameterInvalidJsonValueError as ParameterInvalidJsonValueError
 from ._core.errors import ParameterInvalidProviderError as ParameterInvalidProviderError
 from ._core.errors import ParameterInvalidValueError as ParameterInvalidValueError
-from ._core.errors import PoolMaxConnectionsError as PoolMaxConnectionsError
+from ._core.errors import PoolAlreadyStarted as PoolAlreadyStarted
+from ._core.errors import PoolClosed as PoolClosed
+from ._core.errors import PoolTimeout as PoolTimeout
 from ._core.errors import PySQLXError as PySQLXError
 from ._core.errors import QueryError as QueryError
 from ._core.errors import RawCmdError as RawCmdError
