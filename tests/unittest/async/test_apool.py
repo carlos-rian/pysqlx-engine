1d5cb28858943ce95f36ac862cc2ef75a45597e0
carlos.rian <carlos.rian@quartile.com>
2024-10-04 19:53:13 -0300

    Refactor connection pool classes for better code organization and readability

--- tests/unittest/async/test_apool.py
+++ tests/unittest/async/test_apool.py
@@ -1,9 +1,16 @@
 import asyncio
+import unittest.mock
 
 import pytest
 
 from pysqlx_engine import PySQLXEnginePool
-from pysqlx_engine._core.errors import PoolClosedError
+from pysqlx_engine._core.errors import (
+	PoolAlreadyClosedError,
+	PoolAlreadyStartedError,
+	PoolClosedError,
+	PoolTimeoutError,
+)
+from pysqlx_engine._core.pool.apool import Monitor
 from tests.common import MSSQL_URI, MYSQL_URI, PGSQL_URI, SQLITE_URI
 
 
@@ -16,7 +23,7 @@ async def get_pool(uri: str, min_size=3, check_interval=1, keep_alive=60 * 15, *
 @pytest.mark.asyncio
 @pytest.mark.parametrize("uri", [SQLITE_URI, PGSQL_URI, MSSQL_URI, MYSQL_URI])
 async def test_pool_initialization(uri: str):
-	pool = await get_pool(uri)
+	pool = await get_pool(uri, check_interval=10)
 	assert pool._min_size == 3, "Min size should be 3"
 	await asyncio.sleep(1)
 	assert pool._opened is True
@@ -111,4 +118,158 @@ async def test_renew_connection(uri: str):
 	conn_ids.sort()
 	new_conn_ids.sort()
 	assert conn_ids != new_conn_ids, "Connections should be renewed"
+	await pool.stop()
+
+
+def test_pool_initialization_raise():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, max_size=None, min_size=10)
+	assert pool._max_size == 10, "Max size should be 10"
+	assert pool._min_size == 10, "Min size should be 10"
+	assert "at 0x" in repr(pool)
+
+	with pytest.raises(ValueError):
+		pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=0)
+
+	with pytest.raises(ValueError):
+		pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=-1)
+
+	with pytest.raises(ValueError):
+		pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=10, max_size=5)
+
+	with pytest.raises(AssertionError):
+		pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, conn_timeout=0)
+
+	with pytest.raises(AssertionError):
+		pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, keep_alive=0)
+
+	with pytest.raises(AssertionError):
+		pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, check_interval=0)
+
+
+@pytest.mark.asyncio
+async def test_monitor_break_if_queue_is_empty():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1)
+	pool._opened = True
+	with unittest.mock.patch.object(pool._pool, "get_nowait", side_effect=asyncio.QueueEmpty):
+		monitor = Monitor(pool=pool)
+		pool._pool.put_nowait("test")
+		assert pool._name in repr(monitor)
+		t = asyncio.create_task(monitor.run())
+		await asyncio.sleep(1)
+		pool._opened = False
+		await t
+
+
+@pytest.mark.asyncio
+async def test_monitor_break_if_size_is_above_max():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1)
+	pool._opened = True
+	pool._pool.put_nowait(await pool._new_conn_unchecked())
+	assert pool._size == 1
+	assert pool._opened is True
+
+	monitor = Monitor(pool=pool)
+	pool._size = 2
+	t = asyncio.create_task(monitor.run())
+	await asyncio.sleep(1)
+	pool._opened = False
+	await t
+
+
+@pytest.mark.asyncio
+async def test_pool_stop_raise():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1)
+	with pytest.raises(PoolAlreadyClosedError):
+		await pool.stop()
+
+
+@pytest.mark.asyncio
+async def test_pool_timeout_raise():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1, conn_timeout=1)
+	await pool.start()
+	await asyncio.sleep(1)
+	async with pool.connection() as conn:
+		assert conn is not None, "Connection should not be None"
+		assert pool._pool.qsize() < 1, "Connection should be removed from the pool"
+		with pytest.raises(PoolTimeoutError):
+			async with pool.connection() as _:
+				...
+	await pool.stop()
+
+
+@pytest.mark.asyncio
+async def test_pool_raise_during_connection():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1)
+	await pool.start()
+	await asyncio.sleep(1)
+	with pytest.raises(ZeroDivisionError):
+		async with pool.connection() as conn:
+			assert conn is not None, "Connection should not be None"
+			assert pool._pool.qsize() < 1, "Connection should be removed from the pool"
+			1 / 0
+
+	await pool.stop()
+
+
+@pytest.mark.asyncio
+async def test_pool_on_transaction():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1, check_interval=10)
+	await pool.start()
+	await asyncio.sleep(1)
+	async with pool.connection() as conn:
+		assert conn is not None, "Connection should not be None"
+		assert pool._pool.qsize() < 1, "Connection should be removed from the pool"
+		await conn.start_transaction()
+	pool._pool.qsize() == 0
+	await pool.stop()
+
+
+@pytest.mark.asyncio
+async def test_pool_start_twice():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1)
+	await pool.start()
+	with pytest.raises(PoolAlreadyStartedError):
+		await pool.start()
+	pool.stop()
+
+
+@pytest.mark.asyncio
+async def test_pool_start_generate_raise():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1)
+	with unittest.mock.patch.object(pool._pool, "put", side_effect=Exception):
+		with pytest.raises(Exception):
+			await pool.start()
+
+
+@pytest.mark.asyncio
+async def test_pool_get_conn():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1)
+	await pool.start()
+	with unittest.mock.patch.object(pool._semaphore, "acquire", side_effect=asyncio.TimeoutError):
+		with pytest.raises(PoolTimeoutError):
+			await pool._get_conn()
+
+	await pool.stop()
+
+
+@pytest.mark.asyncio
+async def test_pool_get_ready_conn():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1)
+	await pool.start()
+	with unittest.mock.patch.object(pool._pool, "get", side_effect=asyncio.TimeoutError):
+		with pytest.raises(PoolTimeoutError):
+			async with pool.connection() as _:
+				await pool._get_ready_conn()
+
+	await pool.stop()
+
+
+@pytest.mark.asyncio
+async def test_pool__put_conn_unchecked_raise():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=10)
+	await pool.start()
+	with unittest.mock.patch.object(pool._pool, "put", side_effect=asyncio.QueueFull):
+		conn = await pool._new_conn_unchecked()
+		await pool._put_conn_unchecked(conn)
+
 	await pool.stop()
--- tests/unittest/sync/test_pool.py
+++ tests/unittest/sync/test_pool.py
@@ -1,9 +1,18 @@
+import queue
+import threading
 import time
+import unittest.mock
 
 import pytest
 
 from pysqlx_engine import PySQLXEnginePoolSync as PySQLXEnginePool
-from pysqlx_engine._core.errors import PoolClosedError
+from pysqlx_engine._core.errors import (
+	PoolAlreadyClosedError,
+	PoolAlreadyStartedError,
+	PoolClosedError,
+	PoolTimeoutError,
+)
+from pysqlx_engine._core.pool.pool import Monitor
 from tests.common import MSSQL_URI, MYSQL_URI, PGSQL_URI, SQLITE_URI
 
 
@@ -15,7 +24,7 @@ def get_pool(uri: str, min_size=3, check_interval=1, keep_alive=60 * 15, **kwarg
 
 @pytest.mark.parametrize("uri", [SQLITE_URI, PGSQL_URI, MSSQL_URI, MYSQL_URI])
 def test_pool_initialization(uri: str):
-	pool = get_pool(uri=uri)
+	pool = get_pool(uri=uri, check_interval=10)
 	assert pool._min_size == 3, "Min size should be 3"
 	time.sleep(1)
 	assert pool._opened is True
@@ -105,4 +114,147 @@ def test_renew_connection(uri: str):
 	conn_ids.sort()
 	new_conn_ids.sort()
 	assert conn_ids != new_conn_ids, "Connections should be renewed"
+	pool.stop()
+
+
+def test_pool_initialization_raise():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, max_size=None, min_size=10)
+	assert pool._max_size == 10, "Max size should be 10"
+	assert pool._min_size == 10, "Min size should be 10"
+	assert "at 0x" in repr(pool)
+
+	with pytest.raises(ValueError):
+		pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=0)
+
+	with pytest.raises(ValueError):
+		pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=-1)
+
+	with pytest.raises(ValueError):
+		pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=10, max_size=5)
+
+	with pytest.raises(AssertionError):
+		pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, conn_timeout=0)
+
+	with pytest.raises(AssertionError):
+		pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, keep_alive=0)
+
+	with pytest.raises(AssertionError):
+		pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, check_interval=0)
+
+
+def test_monitor_break_if_queue_is_empty():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1)
+	pool._opened = True
+	with unittest.mock.patch.object(pool._pool, "get_nowait", side_effect=queue.Empty):
+		monitor = Monitor(pool=pool)
+		assert pool._name in repr(monitor)
+		pool._pool.put_nowait("test")
+		t = threading.Thread(target=monitor.run, daemon=True)
+		t.start()
+		time.sleep(1)
+		pool._opened = False
+		t.join()
+
+
+def test_monitor_break_if_size_is_above_max():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1)
+	pool._opened = True
+	pool._pool.put_nowait(pool._new_conn_unchecked())
+	assert pool._size == 1
+	assert pool._opened is True
+
+	monitor = Monitor(pool=pool)
+	pool._size = 2
+	t = threading.Thread(target=monitor.run, daemon=True)
+	t.start()
+	time.sleep(1)
+	pool._opened = False
+	t.join()
+
+
+def test_pool_stop_raise():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1)
+	with pytest.raises(PoolAlreadyClosedError):
+		pool.stop()
+
+
+def test_pool_timeout_raise():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1, conn_timeout=1)
+	pool.start()
+	time.sleep(1)
+	with pool.connection() as conn:
+		assert conn is not None, "Connection should not be None"
+		assert pool._pool.qsize() < 1, "Connection should be removed from the pool"
+		with pytest.raises(PoolTimeoutError):
+			with pool.connection() as _:
+				...
+	pool.stop()
+
+
+def test_pool_raise_during_connection():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1)
+	pool.start()
+	time.sleep(1)
+	with pytest.raises(ZeroDivisionError):
+		with pool.connection() as conn:
+			assert conn is not None, "Connection should not be None"
+			assert pool._pool.qsize() < 1, "Connection should be removed from the pool"
+			1 / 0
+
+	pool.stop()
+
+
+def test_pool_on_transaction():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1, check_interval=10)
+	pool.start()
+	time.sleep(1)
+	with pool.connection() as conn:
+		assert conn is not None, "Connection should not be None"
+		assert pool._pool.qsize() < 1, "Connection should be removed from the pool"
+		conn.start_transaction()
+	pool._pool.qsize() == 0
+	pool.stop()
+
+
+def test_pool_start_twice():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1)
+	pool.start()
+	with pytest.raises(PoolAlreadyStartedError):
+		pool.start()
+	pool.stop()
+
+
+def test_pool_start_generate_raise():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1)
+	with unittest.mock.patch.object(pool._pool, "put", side_effect=Exception):
+		with pytest.raises(Exception):
+			pool.start()
+
+
+def test_pool_get_conn():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1)
+	pool.start()
+	with unittest.mock.patch.object(pool._semaphore, "acquire", return_value=None):
+		with pytest.raises(PoolTimeoutError) as e:
+			pool._get_conn()
+	assert "Timeout waiting for a connection semaphore" in str(e.value)
+	pool.stop()
+
+
+def test_pool_get_ready_conn():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=1)
+	pool.start()
+	with unittest.mock.patch.object(pool._pool, "get", side_effect=queue.Empty):
+		assert pool._get_ready_conn() is None
+
+	pool.stop()
+
+
+def test_pool__put_conn_unchecked_raise():
+	pool = PySQLXEnginePool(uri=SQLITE_URI, min_size=1, max_size=10)
+	pool.start()
+	with unittest.mock.patch.object(pool._pool, "put", side_effect=queue.Full):
+		conn = pool._new_conn_unchecked()
+		pool._put_conn_unchecked(conn)
+
 	pool.stop()
